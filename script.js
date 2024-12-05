12223174c0cb82dbd1ff240d1a62f466de4b1fb3
Karay2006 <karay1@cps.edu>
2024-12-05 16:48:31 +0000

    x

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let  left = document.querySelector('.left');
-let  right = document.querySelector('.right');
-let  leftRight = document.querySelector('.leftRight');
+let  left = document.querySelector(".left");
+let  right = document.querySelector(".right");
+// let  leftRight = document.querySelector('.leftRight');
+// let leftChoice= document.querySelector('.LeftChoice');
+let running = document.querySelector(".running");
+let start = document.querySelector(".start");
 
-let  pizza = document.querySelector('.pizza');
-pizza.style.display ="none";
-
-left.addEventListener('click', function() {
-
-    leftRight.style.display ="none";
-    left.style.display ="none";
-    right.style.display ="none";
-   
-    pizza.style.display ="block";
-
+console.log("hello");
+start.addEventListener('click', function(){
+    start.style.display= "none";
 });
+
+// left.addEventListener('click', function() {
+//     start.style.display = "none";
+// });
+
+// start.addEventListener('click',function() {
+//     console.log("you choosed left");
+//     left.style.display ="block";
+//     right.style.display = "block";
+//     leftRight.style.display ="block";
+//  leftChoice.style.display ="block";
+};
